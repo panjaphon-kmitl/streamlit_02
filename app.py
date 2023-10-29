3f462c5930559913bd73c1926983183684eb12a5
panjaphon <66056053@kmitl.ac.th>
2023-10-29 11:59:57 +0700

    Add query

--- app.py
+++ app.py
@@ -11,37 +11,48 @@ st.write("""
   เราจะลองทำ San Francisco Dataset กันดู
 """)
 
-df = pd.read_csv('trees.csv')
-df2 = pd.DataFrame(
-    df.groupby(['dbh']).count()['tree_id']
+trees_df = pd.read_csv('trees.csv')
+
+owners = st.sidebar.multiselect(
+    "Tree Owner Filter",
+    trees_df['caretaker'].unique()
 )
-df2.columns = ['tree_count']
+
+query = '(index == index or index != index)'
+
+if owners != []:
+    query += ' and caretaker in @owners'
+
+df_dbh_grouped = pd.DataFrame(
+    trees_df.query(query).groupby(['dbh']).count()['tree_id']
+)
+df_dbh_grouped.columns = ['tree_count']
 
 st.divider()
-#
+
 # col1,col2,col3 = st.columns(3)
 # with col1:
 #     st.write('Column1')
-#     st.line_chart(df2)
+#     st.line_chart(df_dbh_grouped)
 # with col2:
 #     st.write('Column2')
-#     st.bar_chart(df2)
+#     st.bar_chart(df_dbh_grouped)
 # with col3:
 #     st.write('Column3')
-#     st.area_chart(df2)
+#     st.area_chart(df_dbh_grouped)
 #
 # st.divider()
 
 tab1,tab2,tab3 = st.tabs(["Line", "Bar", "Area"])
 with tab1:
     st.write('Column1')
-    st.line_chart(df2)
+    st.line_chart(df_dbh_grouped)
 with tab2:
     st.write('Column2')
-    st.bar_chart(df2)
+    st.bar_chart(df_dbh_grouped)
 with tab3:
     st.write('Column3')
-    st.area_chart(df2)
+    st.area_chart(df_dbh_grouped)
 
 st.caption('กราฟแสดงจำนวนต้นไม้ จัดกลุ่มตามเส้นผ่าศูนย์กลาง')
 st.title('แปรผล')
