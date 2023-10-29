9c1437e1f1efc0464395c3fa83ce81a9c5efc870
panjaphon <66056053@kmitl.ac.th>
2023-10-29 11:39:33 +0700

    Changed file name from main.py to app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.markdown('สวัสดี! *Streamlit*')
+st.title('Layout and Decoration')
+st.write("""
+  เราจะลองทำ San Francisco Dataset กันดู
+""")
+
+df = pd.read_csv('trees.csv')
+df2 = pd.DataFrame(
+    df.groupby(['dbh']).count()['tree_id']
+)
+df2.columns = ['tree_count']
+
+col1,col2,col3 = st.columns(3)
+with col1:
+    st.write('Column1')
+    st.line_chart(df2)
+with col2:
+    st.write('Column2')
+    st.bar_chart(df2)
+with col3:
+    st.write('Column3')
+    st.area_chart(df2)
+
+st.caption('กราฟแสดงจำนวนต้นไม้ จัดกลุ่มตามเส้นผ่าศูนย์กลาง')
+st.title('แปรผล')
+st.write('ส่วนใหญ่ของต้นไม้ใน SF มีเส้นผ่าศูนย์กลาง 3\' (2,721 ต้น)')
